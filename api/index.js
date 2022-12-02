1348a6f8f5b572744c534cc9c5c51a96bf991004
mcguia <amcg10@gmail.com>
2022-12-02 17:14:31 -0500

    vercel config

--- api/index.js
+++ api/index.js
@@ -19,15 +19,6 @@ class App {
     this.app = express();
     this.app.use(cors());
     routes.configureRoutes(this.app, this.config);
-
-    if (process.env.NODE_ENV === "production") {
-      // Serve any static files
-      this.app.use(express.static(path.join(__dirname, "../client/build")));
-      // Handle React routing, return all requests to React app
-      this.app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-      });
-    }
   }
 
   launch(callback) {
